d483b06a5bab2dc5d8e7ca96db80ee8bcf277c6d
Jonathan Ho <jonafanho@gmail.com>
2025-01-17 10:33:21 -0800

    Added scripts

---
+++ script/download_godot.sh
@@ -0,0 +1,25 @@
+﻿#!/bin/bash
+
+download() {
+	version=$(curl -s "https://api.github.com/repos/godotengine/godot-builds/releases" | grep -oPm 1 "\"name\":\s*\"\K[^\"]+")
+	echo "Downloading Godot $version for $1"
+
+	rm -rf "../godot/$1/"
+	mkdir "../godot/$1/"
+
+	curl -Lo godot.zip "https://github.com/godotengine/godot-builds/releases/download/${version}/Godot_v${version}_mono_$2.zip"
+	unzip -q godot.zip -d "temp"
+	mv temp/*/* "../godot/$1"
+
+	for file in "../godot/$1/"Godot_v*_mono_*; do
+		new_name=${file//"v${version}_mono_"/''}
+		mv "$file" "$new_name"
+	done
+
+	rm -r temp
+	rm godot.zip
+}
+
+mkdir "../godot/"
+download linux linux_x86_64
+download windows win64
---
+++ script/download_openscad.sh
@@ -0,0 +1,35 @@
+﻿#!/bin/bash
+
+download() {
+	url=$(curl -s $"https://files.openscad.org/snapshots/.snapshot_win64.js" | grep -oPm 1 "https://files.openscad.org/snapshots/OpenSCAD-\d\d\d\d\.\d\d.\d\d-x86-64.zip")
+	echo "Downloading OpenSCAD for $1"
+
+	rm -rf "../openscad/$1/"
+	mkdir "../openscad/$1/"
+
+	curl -Lo openscad.zip "$url"
+	unzip -q openscad.zip -d "temp"
+	mv temp/*/* "../openscad/$1"
+
+	rm -r temp
+	rm openscad.zip
+}
+
+library() {
+	url=$"https://github.com/BelfrySCAD/BOSL2/archive/refs/heads/master.zip"
+	echo "Downloading BOSL2"
+
+	rm -rf "../openscad/libraries/"
+	mkdir "../openscad/libraries/"
+
+	curl -Lo bosl2.zip "$url"
+	unzip -q bosl2.zip -d "temp"
+	mv temp/*/* "../openscad/libraries"
+
+	rm -r temp
+	rm bosl2.zip
+}
+
+mkdir "../openscad/"
+download windows
+library

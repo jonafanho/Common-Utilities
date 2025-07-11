c03ea9aaf58313465d15bcf6cbe6ad1db03857dc
Jonathan Ho <jonafanho@gmail.com>
2025-07-10 21:08:03 -0700

    Update Godot scripts

--- script/godot_scene_helper.sh
+++ script/godot_scene_helper.sh
@@ -2,13 +2,13 @@
 
 source ./pascal_case.sh
 
-content="namespace Game.Scripts.Tool;public sealed class SceneHelper{"$'\n'
+content="namespace Game.Scripts.Tool;public sealed class SceneHelper(string key){"$'\n'
 
 while IFS= read -r file; do
 	key="${file#../scenes/}"
 	key="${key%.tscn}"
-	content+="public static readonly SceneHelper $(to_pascal_case "$key")=new(){Key=\"res://scenes/$key.tscn\"};"$'\n'
+	content+="public static readonly SceneHelper $(to_pascal_case "$key")=new(\"res://scenes/$key.tscn\");"$'\n'
 done < <(find ../scenes -type f)
 
-content+="public string Key{get;private init;}}"
+content+="public readonly string Key=key;}"
 echo "$content" > ../scripts/tool/SceneHelper.cs
--- script/godot_translation_helper.sh
+++ script/godot_translation_helper.sh
@@ -2,13 +2,13 @@
 
 source ./pascal_case.sh
 
-content="using Godot;namespace Game.Scripts.Tool;public sealed class TranslationHelper{"$'\n'
+content="using Godot;namespace Game.Scripts.Tool;public sealed class TranslationHelper(string key){"$'\n'
 
 while IFS=, read -r key rest; do
 	if [ ${#key} -ge 2 ]; then
-		content+="public static readonly TranslationHelper $(to_pascal_case "$key")=new(){Key=\"$key\"};"$'\n'
+		content+="public static readonly TranslationHelper $(to_pascal_case "$key")=new(\"$key\");"$'\n'
 	fi
 done < <(tail -n +2 ../translations/translations.csv)
 
-content+="public string Key{get;private init;}public string Translate(params string[] arguments){string text=TranslationServer.Translate(Key).ToString();for(int i=0;i<arguments.Length;i++)text=text.Replace(\"{{\"+i+\"}}\",arguments[i]);return text;}}"
+content+="public readonly string Key=key;public string Translate(params string[] arguments){string text=TranslationServer.Translate(Key).ToString();for(int i=0;i<arguments.Length;i++)text=text.Replace(\"{{\"+i+\"}}\",arguments[i]);return text;}}"
 echo "$content" > ../scripts/tool/TranslationHelper.cs
